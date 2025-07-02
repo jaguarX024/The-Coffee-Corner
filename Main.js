ffeab65045f8e24e1720c04bd30bb2b74ddeb2e3
Cherif Haidara <haidaracherif024@gmail.com>
2025-07-02 12:47:21 -0400

    Main page of the website is complete. Now moving to About page

--- Main.js
+++ Main.js
@@ -40,3 +40,56 @@ job_arrow.addEventListener("click",()=>{
     job_arrow.classList.toggle("jobs-arrow");
     
 });
+
+
+//handle screen resize events
+let lastScreenIsLarge= window.innerWidth >= 980;
+window.addEventListener("resize", () => {
+    const isLarge = window.innerWidth >= 980;
+    if (isLarge !== lastScreenIsLarge) {
+        // The screen crossed the 900px boundary!
+        if (isLarge) {
+            // Switched to large screen (>=980px)
+            
+            //closing the hamburger menu when switching to big screens 
+            if (ham_menu.classList.contains("show-menu")){
+                ham_menu.classList.remove("show-menu");
+            }
+        
+            
+        } else {
+            // Switched to small screen (<980px)
+            if (job_typesOptions.classList.contains("show-jobTypes")){
+                job_typesOptions.classList.remove("show-jobTypes");
+                job_arrow.classList.remove("jobs-arrow");
+            }
+        }
+        lastScreenIsLarge = isLarge; // Update the tracker
+    }
+});
+
+//Managing window click events
+document.body.addEventListener("click", (event)=>{
+    if (ham_menu.classList.contains("show-menu")){
+        if (ham_menu.contains(event.target)&& !event.target.closest(".ham-menu-link")){
+            //do nothing
+        }else{
+            if (!ham_icon.contains(event.target)){
+                ham_menu.classList.remove("show-menu");
+            }
+        }
+    }
+
+    //closing jop options slide when clicking out of its bounds
+    if (job_typesOptions.classList.contains("show-jobTypes")){
+        if (job_typesOptions.contains(event.target)&& !event.target.closest(".media3-link")){
+            //do nothing
+        }else{
+            if (!job_arrow.contains(event.target)){
+                job_typesOptions.classList.remove("show-jobTypes");
+                job_arrow.classList.remove("jobs-arrow");
+            }
+        }
+    }
+
+});
